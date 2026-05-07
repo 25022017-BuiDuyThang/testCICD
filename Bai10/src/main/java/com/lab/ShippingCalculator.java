4a31e1184cbc763ef0fca6a6eabee2aee9901dbd
BuiDuyThang <25022017@vnu.edu.vn>
2026-05-07 07:57:22 +0700

    test

--- Bai10/src/main/java/com/lab/ShippingCalculator.java
+++ Bai10/src/main/java/com/lab/ShippingCalculator.java
@@ -1,17 +1,4 @@
-package com.lab;
 
-public class ShippingCalculator {
-
-  public double calculate(double weight, String type) {
-    if (weight <= 0) {
-      throw new IllegalArgumentException("Weight must be positive");
-    }
-    if (type.equals("EXPRESS")) return weight * 5000 + 20000;
-    if (type.equals("STANDARD")) return weight * 3000;
-    throw new IllegalArgumentException("Unknown type: " + type);
-  }
-}
-/*
 package com.lab;
 
 public class ShippingCalculator {
@@ -25,5 +12,3 @@ public class ShippingCalculator {
     throw new IllegalArgumentException("Unknown type: " + type);
   }
 }
-
- */
