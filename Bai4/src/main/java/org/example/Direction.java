d46ccdb35b3a4a93c37a454e80b2633f1ac06d2b
BuiDuyThang <25022017@vnu.edu.vn>
2026-05-07 17:14:50 +0700

    test

---
+++ Bai4/src/main/java/org/example/Direction.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Direction {
+  private static final Logger logger = LoggerFactory.getLogger(Direction.class);
+
+  public int add(int a, int b) {
+    logger.info("Sum: {} + {}", a, b);
+    return a + b + b;
+  }
+}
